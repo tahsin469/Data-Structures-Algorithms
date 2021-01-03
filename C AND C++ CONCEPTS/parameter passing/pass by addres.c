2d4785b3ebd6a18c52805a4b401c53839f5e8977
Md.Tahsinul Islam <mughdaw825@gmail.com>
2021-01-04 04:05:57 +0600

    Add files via upload

---
+++ C AND C++ CONCEPTS/parameter passing/pass by addres.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void swap(int *x,int *y)
+{
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+int main()
+{
+    int a=10,b=20;
+    swap(&a,&b);
+    printf("a=%d\nb=%d",a,b);
+
+    return 0;
+}
+
